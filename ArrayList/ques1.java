01426ce539c99575a629906350cfcae276eb7c46
Ishika Garg <ishikagarg330@gmail.com>
2025-09-03 12:04:00 +0530

    Basic Usage.java

---
+++ ArrayList/ques1.java
@@ -0,0 +1,22 @@
+# Basic Usage
+import java.util.ArrayList;
+class Main {
+    public static void main(String[] args) {
+        ArrayList<String> al = new ArrayList<>();
+        al.add("a");
+        al.add("b");
+        al.add("c");
+        al.add("d");
+        al.add("e");
+        
+        for(int i=0; i<al.size(); i++){
+            System.out.print(al.get(i));
+        }
+        
+        al.set(2,"z");
+        
+        for(int i=0; i<al.size(); i++){
+            System.out.print(al.get(i));
+        }
+    }
+}
